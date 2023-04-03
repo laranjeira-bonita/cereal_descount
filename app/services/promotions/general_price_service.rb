4d9cd178673449596d8a16f5c7142dd5f6d9bef3
shu qi <shu@fetch.ly>
2023-04-03 16:42:40 +0200

    if no promotion applied for ow, just sum the price

--- app/services/promotions/general_price_service.rb
+++ app/services/promotions/general_price_service.rb
@@ -3,8 +3,12 @@ module Promotions
     class << self
       def get_price_from_current_promotion(params)
         promotion = Promotion.where('start_date <= ?', Time.current).where('end_date >= ?', Time.current).order(priority_level: :desc).first
-        promotion_service = promotion.name.split(/\s/).map{|word|word.capitalize}.join + 'Service'
-        "Promotions::#{promotion_service}".constantize.get_price(params)     #calculate
+        if promotion.present?
+          promotion_service = promotion.name.split(/\s/).map{|word|word.capitalize}.join + 'Service'
+          "Promotions::#{promotion_service}".constantize.get_price(params)
+        else
+          params['lineItems'].pluck(:price).map{|n|n.to_f}.sum.round(2)
+        end
       end
     end
   end
