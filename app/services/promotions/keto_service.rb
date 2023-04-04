ce1a4d3b18d44e9eb16a51168f2b2c48422a2ad3
shu qi <shu@fetch.ly>
2023-04-04 14:27:35 +0200

    calculate sum in keto promotion in easier way

--- app/services/promotions/keto_service.rb
+++ app/services/promotions/keto_service.rb
@@ -2,20 +2,26 @@ module Promotions
   class KetoService
     class << self
       def get_price(params)
-        not_keto_product_prices = params['lineItems'].select{|p|!p['collection'].eql?('KETO')}.pluck(:price).map{|n|n.to_f}
-        not_keto_count = not_keto_product_prices.count
-        descount = if not_keto_count.eql? 2
-          not_keto_product_prices.sum * 0.05
-        elsif not_keto_count.eql? 3
-          not_keto_product_prices.sum * 0.1
-        elsif not_keto_count.eql? 4
-          not_keto_product_prices.sum * 0.2
-        elsif not_keto_count >= 5
-          not_keto_product_prices.sum * 0.25
-        end
-        final_price = (params['lineItems'].pluck(:price).map{|n|n.to_f}.sum - descount).round(2)
+        not_keto_products = params['lineItems'].select{|p|!p['collection'].eql?('KETO')}
+        descount = case not_keto_products.count
+          when 0..1
+            0
+          when 2
+            total_price(not_keto_products) * 0.05
+          when 3
+            total_price(not_keto_products) * 0.1
+          when 4
+            total_price(not_keto_products) * 0.2
+          else
+            total_price(not_keto_products) * 0.25
+          end
+        final_price = (total_price(params['lineItems']) - descount).round(2)
         # case params['lineItems'].count
         # end
+      end
+      private
+      def total_price(products)
+        products.sum{|p|p[:price].to_f}
       end
     end
   end
