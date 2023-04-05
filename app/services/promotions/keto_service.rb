71b4169ece39dafb57e5aa8be5fea1891ac9d75b
shu qi <shu@fetch.ly>
2023-04-05 10:56:06 +0200

    return a json with discounted-price

--- app/services/promotions/keto_service.rb
+++ app/services/promotions/keto_service.rb
@@ -5,19 +5,29 @@ module Promotions
         not_keto_products = params['lineItems'].select{|p|!p['collection'].eql?('KETO')}
         descount = case not_keto_products.count
           when 0..1
-            0
+            1
           when 2
-            total_price(not_keto_products) * 0.05
+            0.95
           when 3
-            total_price(not_keto_products) * 0.1
+            0.9
           when 4
-            total_price(not_keto_products) * 0.2
+            0.8
           else
-            total_price(not_keto_products) * 0.25
+            0.75
           end
-        final_price = (total_price(params['lineItems']) - descount).round(2)
-        # case params['lineItems'].count
-        # end
+        total_price = 0
+        line_items = params['lineItems'].map do |product|
+          discounted_price = product['collection'].eql?('KETO') ? product['price'].to_f : (product['price'].to_f * descount).round(2)
+          total_price += discounted_price
+          product.merge({ 'discountedPrice':  discounted_price})
+        end
+        {
+          "cart": {
+            "total_price": total_price.round(2),
+            "reference": params["reference"],
+            "lineItems": line_items
+          }
+        }
       end
       private
       def total_price(products)
